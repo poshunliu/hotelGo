ac60ae164cf69fb9a8b9bcf080bb56718f1c6c37
poshunliu <gmu.pliu9@gmail.com>
2018-08-10 12:40:53 -0400

    added SecurityConfig and modified web.xml

---
+++ src/main/java/com/hotelgo/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.hotelgo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication().withUser("user1")
+                .password("{noop}password").roles("REGISTERED_USER");
+    }
+
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }
+
+
+}
