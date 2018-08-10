ededcccd214b0d42f5cf80b99ee3a8c92a0789c9
poshunliu <gmu.pliu9@gmail.com>
2018-08-10 12:42:07 -0400

    added new package

--- src/main/java/com/hotelgo/config/SecurityConfig.java
+++ src/main/java/com/hotelgo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.hotelgo.config;
 
+import com.hotelgo.extend.security.RestAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,25 +8,41 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+//@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.inMemoryAuthentication().withUser("user1")
-                .password("{noop}password").roles("REGISTERED_USER");
-    }
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
 
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth)
+//            throws Exception {
+//        auth.inMemoryAuthentication().withUser("user1")
+//                .password("{noop}password").roles("REGISTERED_USER");
+//    }
+//
+//
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable()
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin();
+//    }
+       @Autowired
+       public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+              auth.inMemoryAuthentication().withUser("user")
+               .password("{noop}password").roles("USER");
+       }
 
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .anyRequest().authenticated()
+       protected void configure(HttpSecurity http) throws Exception {
+
+             http.csrf().disable().authorizeRequests().antMatchers("/api/users/login","/api/user/login","/api/users/signup","/login").permitAll()
                 .and()
-                .formLogin();
+                .authorizeRequests().antMatchers("/api/**").hasAnyRole("REGISTERED_USER","ADMIN")
+                .and()
+                .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and().formLogin();
     }
-
 
 }
